8ea9fda9501c7a97633420e3f9272c13190c8c5a
sairammohan <sairammohan@gmail.com>
2026-03-30 19:37:48 -0700

    added couple of tests for js and ts

---
+++ ts-js-files/jstest.js
@@ -0,0 +1,6 @@
+let username = 'tomsmith';
+console.log ('before reassignment:', username);
+//let username:string = 'tomsmith';
+username = 42;
+console.log ('After reassignment:', username);
+console.log ('JavaScript did not complain because it allows dynamic typing.');
---
+++ ts-js-files/tstest.ts
@@ -0,0 +1,6 @@
+let username:string = 'tomsmith';
+console.log ('Before reassignment:', username);
+
+username = 'John';
+console.log ('After reassignment:', username);
+console.log ("TypeScript will not complain as string assigned to string  it enforces type checking.");
